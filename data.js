070da6af359c38d894280a3c6bad0e4a17304a33
asma-pathan <asmapathan752@gmail.com>
2022-11-23 17:17:11 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,7 @@
+const students=[
+      {id:1,stdname:"Asma",stdclass:"B.sc"},
+      {id:2,stdname:"Sowjanya",stdclass:"B.sc"},
+      {id:3,stdname:"Jaggu",stdclass:"B.sc"},
+      {id:4,stdname:"Hamini",stdclass:"B.sc"}
+      ]
+module.exports=students
---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const port=2000;
+
+const employee={id:62,name:"Asma",position:"frontend"}
+const students=require("./data.js")
+
+app.use(cors())
+ 
+ app.get("/",(req,res)=>{
+ 	res.send("starting route")
+ })
+ app.get("/demo",(req,res)=>{
+ 	res.send("this is demo route")
+ })
+
+ app.get("/emp",(req,res)=>{
+ 	res.json(employee)
+ })
+ app.get("/students-list",(req,res)=>{
+ 	res.json(students)
+ })
+
+ app.listen(port,()=>console.log("server is running on port 2000"))
